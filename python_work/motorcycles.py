8170aca64d95a42e0a67e3582efbdde44d0314c5
hdvsyu <hdvsyu@foxmail.com>
2018-01-21 16:20:46 +0800

    feat: use pop() method to delete a element and show it

--- python_work/motorcycles.py
+++ python_work/motorcycles.py
@@ -5,8 +5,12 @@ motorcycles[0] = 'ducati'
 print(motorcycles)
 
 motorcycles.append('ducati')
+del motorcycles[0]
 print(motorcycles)
 
 motorcycles.insert(0, 'ducati')
 print(motorcycles)
 
+poped_motorcycle = motorcycles.pop()
+print(motorcycles)
+print(poped_motorcycle)
