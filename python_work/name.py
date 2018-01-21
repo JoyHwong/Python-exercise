72b5cfe6127c3ec3de28ee0bb46727fb75ab98fb
hdvsyu <hdvsyu@foxmail.com>
2018-01-21 15:31:52 +0800

    feat: merge characters by plus sign

--- python_work/name.py
+++ python_work/name.py
@@ -3,3 +3,9 @@ print(name.title())
 print(name.upper())
 print(name.lower())
 
+first_name = "vsyu"
+second_name = "hd"
+full_name = first_name + " " + second_name
+message = "Hello, " + full_name.title() + "!"
+print(message)
+
