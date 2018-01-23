78db92bfc53aa3455d7f2f2a73ca15254c1dee84
hdvsyu <hdvsyu@foxmail.com>
2018-01-23 21:28:01 +0800

    feat: paralla if statement

--- python_work/toppings.py
+++ python_work/toppings.py
@@ -3,3 +3,14 @@ requested_topping = 'mushrooms'
 if requested_topping != 'anchovies':
     print("Hold the anchovies!")
 
+requested_topping_1 = ['mushrooms', 'extra cheese']
+
+if 'mushrooms' in requested_topping_1:
+    print("Adding mushrooms.")
+if 'peperoni' in requested_topping_1:
+    print("Adding pepperoni.")
+if 'extra cheese' in requested_topping_1:
+    print("Adding extra cheese")
+
+print("\nFinished making your pizza!")
+
