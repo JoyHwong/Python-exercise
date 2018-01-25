f59705c8f5dc9d4e5697150185109a26c0f2e7f2
hdvsyu <hdvsyu@foxmail.com>
2018-01-25 11:46:05 +0800

    feat: define a object

--- python_work/alien.py
+++ python_work/alien.py
@@ -1,17 +1,5 @@
-alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
-print("Original x-position: " + str(alien_0['x_position']))
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0)
 
-# move alien right toward
-# according to the speed of alien decide how far it need move
-if alien_0['speed'] == 'slow':
-    x_increment = 1
-elif alien_0['speed'] == 'medium':
-    x_increment = 2
-else:
-    # the alien's speed is so fast
-    x_increment = 3;
-
-# new location is the old location added the increment
-alien_0['x_position'] = alien_0['x_position'] + x_increment
-print("New x-position: " + str(alien_0['x_position']))
-
+del alien_0['points']
+print(alien_0)
