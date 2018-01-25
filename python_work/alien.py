45e58b3d4af83ac709f90e55a175bcacf4e77dbe
hdvsyu <hdvsyu@foxmail.com>
2018-01-25 11:10:58 +0800

    feat: access the value of dictionary

--- python_work/alien.py
+++ python_work/alien.py
@@ -1,5 +1,4 @@
 alien_0 = {'color': 'green', 'points': 5}
 
-print(alien_0['color'])
-print(alien_0['points'])
-
+new_points = alien_0['points']
+print("You ust earned " + str(new_points) + " points!")
