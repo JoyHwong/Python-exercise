722cb7de5b6f91f6df68486475d3f42a1a28d04b
hdvsyu <hdvsyu@foxmail.com>
2018-01-25 11:25:07 +0800

    feat: alien move

--- python_work/alien.py
+++ python_work/alien.py
@@ -1,5 +1,17 @@
-alien_0 = {'color': 'green'}
-print("The alien is " + alien_0['color'] + ".")
+alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
+print("Original x-position: " + str(alien_0['x_position']))
 
-alien_0['color'] = 'yellow'
-print("The alien is now " + alien_0['color'] + ".")
+# move alien right toward
+# according to the speed of alien decide how far it need move
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    # the alien's speed is so fast
+    x_increment = 3;
+
+# new location is the old location added the increment
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+print("New x-position: " + str(alien_0['x_position']))
+
