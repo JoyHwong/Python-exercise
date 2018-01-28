732fb574f0a9829ac49838f52698b623772a288a
hdvsyu <hdvsyu@foxmail.com>
2018-01-28 11:33:00 +0800

    feat: combine use function and whil loop

--- python_work/formatted_name.py
+++ python_work/formatted_name.py
@@ -12,3 +12,17 @@ print(musician)
 musician = get_formatted_name('johb', 'hooker', 'lee')
 print(musician)
 
+# this is a infinite loop
+while True:
+    print("\nPlease tell me your name:")
+    print("(enter 'q' at any time to quit)")
+    f_name = input("First name: ")
+    if f_name == 'q':
+        break
+
+    l_name = input("Last name: ")
+    if l_name == 'q':
+        break
+
+    formatted_name = get_formatted_name(f_name, l_name)
+    print("\nHello, " + formatted_name + "!")
