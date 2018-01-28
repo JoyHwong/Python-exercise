78f0dc847a50e86e138098fa8e8556bc64a3388b
hdvsyu <hdvsyu@foxmail.com>
2018-01-28 10:28:09 +0800

    feat: define a function

--- python_work/greeter.py
+++ python_work/greeter.py
@@ -4,3 +4,9 @@ prompt += "\nWhat is your first name? "
 name = input(prompt)
 print("\nHello, " + name + "!")
 
+def greet_user():
+    """Show simple hello"""
+    print("Hello!")
+
+greet_user()
+
