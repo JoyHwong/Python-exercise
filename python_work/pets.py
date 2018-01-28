fc5d627b6f7b00ac04071b90e5d54cee9aebad43
hdvsyu <hdvsyu@foxmail.com>
2018-01-28 10:46:33 +0800

    feat: use location real parameter

--- python_work/pets.py
+++ python_work/pets.py
@@ -6,3 +6,11 @@ while 'cat' in pets:
 
 print(pets)
 
+def describe_pet(animal_type, pet_name):
+    """Show the pet's information"""
+    print("\nI have a " + animal_type + ".")
+    print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+
+describe_pet('hasmster', 'harry')
+describe_pet('dog', 'willie')
+
