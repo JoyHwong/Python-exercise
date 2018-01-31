2c9842a3812552f6fc77a9a90860be3f816d54f9
hdvsyu <hdvsyu@foxmail.com>
2018-01-31 20:51:10 +0800

    feat: instant parameter

--- python_work/pizza.py
+++ python_work/pizza.py
@@ -11,3 +11,11 @@ print("You ordered a " + pizza['crust'] + "-crust pizza " +
 for topping in pizza['toppings']:
     print("\t" + topping)
 
+def make_pizza(size, *toppings):
+    print("\nMaking a " + str(size) + "-inch pizza with the following toppings:")
+    for topping in toppings:
+        print("- " + topping)
+
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
