3a53497de6bbbd7091e5e43d56e2270aa44be880
hdvsyu <hdvsyu@foxmail.com>
2018-02-01 14:29:35 +0800

    feat: import module

--- python_work/pizza.py
+++ python_work/pizza.py
@@ -1,21 +1,5 @@
-# hold information for pizza
-pizza = {
-    'crust': 'thick',
-    'toppings': ['mushrooms', 'extra cheese'],
-    }
-
-# introduce pizza
-print("You ordered a " + pizza['crust'] + "-crust pizza " + 
-        "with the following toppings:")
-
-for topping in pizza['toppings']:
-    print("\t" + topping)
-
 def make_pizza(size, *toppings):
     print("\nMaking a " + str(size) + "-inch pizza with the following toppings:")
     for topping in toppings:
         print("- " + topping)
-
-make_pizza(16, 'pepperoni')
-make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
 
