9541d7097ff7450b69511e7ec831781b7e23f61b
hdvsyu <hdvsyu@foxmail.com>
2018-02-03 12:43:59 +0800

    refactor: use setting class to hold setting

---
+++ alien_invasion/settings.py
@@ -0,0 +1,10 @@
+class Settings():
+    """A class hold all the settings of the game"""
+
+    def __init__(self):
+        """init game's setting"""
+        # screen setting
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
