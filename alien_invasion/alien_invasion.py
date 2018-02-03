2d62e11a6ca4a8bbe5dfdad9ab0cfe36c5024f76
hdvsyu <hdvsyu@foxmail.com>
2018-02-03 14:24:51 +0800

    refactor: use module game_functions

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,9 +1,8 @@
-import sys
-
 import pygame
 
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
     pygame.init()
@@ -15,14 +14,8 @@ def run_game():
 
     while True:
         
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-
-        pygame.display.flip()
+        gf.check_events()
+        gf.update_screen(ai_settings, screen, ship)
 
 run_game()
 
