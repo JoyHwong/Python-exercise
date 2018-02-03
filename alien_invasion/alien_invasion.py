43b015c310fef8ce551b5e79b6cee3a90d05f4ce
hdvsyu <hdvsyu@foxmail.com>
2018-02-03 13:45:30 +0800

    feat: create Ship class

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -3,12 +3,15 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 def run_game():
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    ship = Ship(screen)
 
     while True:
         
@@ -17,6 +20,7 @@ def run_game():
                 sys.exit()
 
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
 
         pygame.display.flip()
 
