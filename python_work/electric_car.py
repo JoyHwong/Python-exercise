011be70f40fbf1ad70828dd13fc014b2806c1ad0
hdvsyu <hdvsyu@foxmail.com>
2018-02-05 11:20:55 +0800

    feat: import class from module

--- python_work/electric_car.py
+++ python_work/electric_car.py
@@ -44,8 +44,3 @@ class ElectricCar(Car):
         super().__init__(make, model, year)
         self.battery = Battery()
 
-my_tesla = ElectricCar('tesla', 'model s', 2016)
-print(my_tesla.get_descriptive_name())
-my_tesla.battery.describe_battery()
-my_tesla.battery.get_range()
-
